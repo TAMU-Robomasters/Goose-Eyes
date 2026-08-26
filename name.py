9aa73d930c22fda8e78312015c6b1a322a76bd47
RibizliRoka <lauraszabo777@gmail.com>
2026-08-25 20:21:25 -0500

    I don't think the pull request works for me

--- name.py
+++ name.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hi this is laura uv run main.py works and I hope I did the rest correctly")
+    print("Hi this is laura uv run main.py works, but I don't think the pull request works for me because there is no option to create a pull request after the compare branches part")
 
 
 if __name__ == "__main__":
