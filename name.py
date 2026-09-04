3d3c66877159a4f1d4c96ce9014891e412c362fb
RibizliRoka <lauraszabo777@gmail.com>
2026-09-03 19:23:53 -0500

    new version

--- name.py
+++ name.py
@@ -1,5 +1,7 @@
 def main():
-    print("Hi this is laura uv run main.py works, but I don't think the pull request works for me because there is no option to create a pull request after the compare branches part")
+    print(
+        "Hi this is laura uv run main.py works, but I don't think the pull request works for me because there is no option to create a pull request after the compare branches part"
+    )
 
 
 if __name__ == "__main__":
